76c791830a37c8be3fd052876e744dc951de27a5
Umera <umeramalek796@gmail.com>
2021-11-13 22:14:00 -0800

    send data to html

--- routes/home.js
+++ routes/home.js
@@ -1,3 +1,17 @@
 // router and path
 const router = require('express').Router();
 const path = require('path');
+
+outer.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+router.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'));
+});
+
+router.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html'));
+});
+
+module.exports = router;
