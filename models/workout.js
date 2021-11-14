29795f7b887de4b24523bc2110975106dd8d3b51
Umera <umeramalek796@gmail.com>
2021-11-13 21:08:26 -0800

    added workout models

--- models/workout.js
+++ models/workout.js
@@ -5,15 +5,32 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-    name: {
-        type: String,
-        trim: true,
-        required: "Enter a name for transaction"
-      },
-      exercises: [{
+    exercises: [{
         type: {
             type: String,
             trim: true,
             required: 'Exercise type required',
         },
-
+        name: {
+            type: String,
+            trim: true,
+            required: "Exercise name required"
+        },
+        duration: {
+            type: Number,
+            required: "Exercise duration required"
+        },
+        weight: {
+            type: Number,
+        },
+        sets: {
+            type: Number,
+        },
+        reps: {
+            type: Number,
+        },
+        distance: {
+            type: Number
+        }
+    }]
+}); 
