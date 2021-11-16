bddf508c0448c9543f6e87f3b59357aff09ee94b
Umera <umeramalek796@gmail.com>
2021-11-15 19:23:01 -0800

    connection to mongo database

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+// connect to the port 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+// express middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+
+// connect to mongo database
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./routes"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
